14fa90c742b9af7ddf30943f77f966ccbbdfb8cc
tuananh009 <cumin1812@gmail.com>
2023-04-27 11:01:52 +0700

    second

--- backend/src/main/java/com/web/demohrm/controller/UserController.java
+++ backend/src/main/java/com/web/demohrm/controller/UserController.java
@@ -1,13 +1,23 @@
 package com.web.demohrm.controller;
 
+import com.web.demohrm.model.User;
+import com.web.demohrm.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class UserController {
+    @Autowired
+    UserService userService;
 
     @GetMapping("/home")
-    public String home() {
+    public String home(Model m) {
+        List<User> list = userService.listUser();
+        m.addAttribute("listUser",list);
         return "home";
     }
 }
